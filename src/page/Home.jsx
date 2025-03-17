1ff26f38b3faff840e84df5b6ff9c02fe2887be5
parinTNK <parin.tnk@gmail.com>
2025-03-17 16:37:15 +0700

    feat: add Footer component with social links and responsive design

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,12 +1,14 @@
 import HeroSection from '@/components/HeroSection'
 import Navbar from '@/components/Navbar'
 import React from 'react'
+import Footer from '@/components/Footer'
 
 function Home() {
   return (
     <>
     <Navbar/>
     <HeroSection/>
+    <Footer/>
     </>
   )
 }
