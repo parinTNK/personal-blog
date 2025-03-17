31a802974fbb1d9283f88e589ce542a1cec0c7f8
parinTNK <parin.tnk@gmail.com>
2025-03-17 16:17:41 +0700

    eat: add HeroSection component with responsive layout

---
+++ src/page/Home.jsx
@@ -0,0 +1,14 @@
+import HeroSection from '@/components/HeroSection'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <Navbar/>
+    <HeroSection/>
+    </>
+  )
+}
+
+export default Home
