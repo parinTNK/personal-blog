79fc6e0d4bd4875395783750fbb6a1d6f4aee08c
parinTNK <parin.tnk@gmail.com>
2025-03-20 13:52:00 +0700

    feat: Implement dynamic category filtering and rendering for blog posts with default Highlight selection and enhanced CardPost component

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,12 +2,14 @@ import HeroSection from '@/components/HeroSection'
 import Navbar from '@/components/Navbar'
 import React from 'react'
 import Footer from '@/components/Footer'
+import Articles from '@/components/Articles'
 
 function Home() {
   return (
     <>
     <Navbar/>
     <HeroSection/>
+    <Articles/>
     <Footer/>
     </>
   )
