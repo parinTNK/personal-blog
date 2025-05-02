e5e6378283c7883fab967f87e1768be645d1748a
parinTNK <parin.tnk@gmail.com>
2025-05-02 14:12:55 +0700

    feat: cors setting

--- server/server.mjs
+++ server/server.mjs
@@ -25,17 +25,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: (origin, callback) => {
-    // console.log('Request Origin:', origin);
-    // console.log('Allowed CLIENT_URL:', process.env.CLIENT_URL);
-    // console.log('Is Origin Allowed?', !origin || allowedOrigins.includes(origin));
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-     
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*',
   credentials: true
 }));
 
